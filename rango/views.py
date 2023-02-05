cd4a949a1b2b12f3785500018cd6006ec453aaf1
Abdulrahman Alshammari <2741778A@student.gla.ac.uk>
2023-02-05 05:54:15 +0000

    ch8

--- rango/views.py
+++ rango/views.py
@@ -61,7 +61,12 @@ def show_category(request, category_name_slug):
     return render(request, 'rango/category.html', context=context_dict)
 
 def about(request):
-    return render(request, 'rango/about.html')
+# prints out whether the method is a GET or a POST
+    print(request.method)
+# prints out the user name, if no one is logged in it prints `AnonymousUser`
+    print(request.user)
+    return render(request, 'rango/about.html', {})
+
 
 def add_category(request):
     form = CategoryForm()
